cbe8ea73d3faf319284611c43b430c9b17d3e6d3
iPaykan <ibiaqwert@gmail.com>
2015-08-11 16:43:43 +0300

    uAPImodule v. 1.1

--- uAPImodule.php
+++ uAPImodule.php
@@ -1,29 +1,21 @@
-<?php
+<?php error_reporting( E_ERROR );
 /* 
-        Версия 1.0 
+        Версия 1.1 
         Дата создания: 9 февраля 02:00
+        Дата обновления 1.1: 14 июля 2015
         Информация о модуле: http://api.ucoz.net/ru/manual/module
         По всем вопросам вы можете обратиться на api@ucoz.net
 */
-
 //setup для работы с uApi
- $oauth_nonce = md5(microtime().mt_rand()); //не изменять
- //$oauth_nonce2 = md5(microtime().mt_rand()); //не изменять
- $timestamp = time(); //не изменять
- $sig_method = 'HMAC-SHA1'; //не изменять
- $oauth_version = '1.0'; //не изменять
-
- /* $consumer_key = 'Nvw3Wm7v1bN8';
- $consumer_secret = 'Tu.yUtCw9ygRQPJFrDpgHurMeWn7q7';
- $oauth_token = 'Ugqe53bimpieTA4wZoSPPfWgakL.v5zjRREXl4RJ';
- $oauth_token_secret = 'yRylogtLxZ4a0.gSdGRhRhn443ZVH422cqEVuGTx';
- $main_url = 'http://alphatest-6347.ucoz.ru/uapi'; //нужно указать с http:// и с uapi. Например: http://alphatest-0027.ucoz.ru/uapi */
-
- /*$consumer_key = '';
- $consumer_secret = '';
- $oauth_token = '';
- $oauth_token_secret = '';
- $main_url = '';*/
+$oauth_nonce= md5(microtime().mt_rand()); //не изменять
+$timestamp = time(); //не изменять
+$sig_method = 'HMAC-SHA1'; //не изменять
+$oauth_version = '1.0'; //не изменять
+$consumer_key = '';
+$consumer_secret = '';
+$oauth_token = '';
+$oauth_token_secret = '';
+$main_url = ''; //нужно указать с http:// и с uapi. Например: http://yourwebsite.ucoz.ru/uapi
 
 //формирование setup закончено
 
@@ -34,73 +26,74 @@
 $method может принимать вид POST, GET, DELETE, PUT. 
 $request_url указывает куда делать запрос
 $parametrs параметры к запросу
-$format в каком виде получить данные (xml/json/)
+$format не поддерживается, создан для будуших доработок (todo)
 */
 
 function uAPIModule ($request_url, $method, $parametrs, $format) {
     global $basestring, $consumer_key, $oauth_nonce, $sig_method, $timestamp, $oauth_token, $oauth_version, $hash_key, $oauth_signature, $consumer_secret, $oauth_token_secret, $main_url;
 
     //начинается формирование подписи для правильного запроса
-$request_url = $main_url.mb_strtolower(trim($request_url));
-$method = mb_strtoupper($method);
-$basestring = str_replace('+', '%20', http_build_query($parametrs));
-$basestring = $method.'&'.urlencode($request_url).'&'.urlencode($basestring);
-$hash_key = $consumer_secret.'&'.$oauth_token_secret;
-$oauth_signature = urlencode(trim(base64_encode(hash_hmac('sha1', $basestring, $hash_key, true))));
-$parametrs_forurl = http_build_query($parametrs);
-$url = $request_url.'?oauth_signature='.$oauth_signature;
-$url_for = $request_url.'?'.$parametrs_forurl.'&oauth_signature='.$oauth_signature;
+    $request_url = $main_url.mb_strtolower(trim($request_url));
+    $method = mb_strtoupper($method);
+    $parametrs2 = $parametrs;
+    $parametrs = str_replace('@', '', $parametrs);
+    $basestring = str_replace('+', '%20', http_build_query($parametrs));
+    $basestring = $method.'&'.urlencode($request_url).'&'.urlencode($basestring);
+    $hash_key = $consumer_secret.'&'.$oauth_token_secret;
+    $oauth_signature = urlencode(trim(base64_encode(hash_hmac('sha1', $basestring, $hash_key, true))));
+    $parametrs_forurl = http_build_query($parametrs);
+    $url = $request_url.'?oauth_signature='.$oauth_signature;
+    $url_for = $request_url.'?'.$parametrs_forurl.'&oauth_signature='.$oauth_signature;
 
-$curl = curl_init();
-switch ($method) {
+    $curl = curl_init();
+    switch ($method) {
 
-  case 'GET':
-  $parametrs = http_build_query($parametrs);
-    curl_setopt($curl, CURLOPT_URL, $url_for);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-    $uAPIcurlresult = curl_exec($curl);
-   return $uAPIcurlresult;
-   break;
-  
-  case 'POST':
-  $parametrs = http_build_query($parametrs);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $parametrs);
-    $uAPIcurlresult = curl_exec($curl);
-    return $uAPIcurlresult;
+        case 'GET':
+            curl_setopt($curl, CURLOPT_URL, $url_for);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+            $uAPIcurlresult = curl_exec($curl);
+            return $uAPIcurlresult;
+            break;
 
-    break;
+        case 'POST':
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $parametrs2);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_VERBOSE, true);
+            $uAPIcurlresult = curl_exec($curl);
+            return $uAPIcurlresult;
+            break;
 
-    case 'PUT':
-      $curl = curl_init($url);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
-      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $parametrs_forurl);
-      $uAPIcurlresult = curl_exec($curl);
-      return $uAPIcurlresult;
-    break;
+        case 'PUT':
+            $curl = curl_init($url);
 
-    case 'DELETE':
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $parametrs2);
+            $uAPIcurlresult = curl_exec($curl);
+            return $uAPIcurlresult;
+            break;
 
-curl_setopt($curl, CURLOPT_URL, $url_for);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
-    $uAPIcurlresult = curl_exec($curl);
-  return $uAPIcurlresult;
+        case 'DELETE':
+            curl_setopt($curl, CURLOPT_URL, $url_for);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            $uAPIcurlresult = curl_exec($curl);
+            return $uAPIcurlresult;
 
-    break;
+            break;
 
 
-  default:
-    echo 'Неизвестный метод передачи данных!';
-    break;
+        default:
+            echo 'Неизвестный метод передачи данных!';
+            break;
 
-    
-}
+
+    }
     curl_close($curl);
 }
 ?>
